2c2d2c1a67251c5316ce22ccaee87652a1cabad0
rikachka <rikachka1@yandex.ru>
2020-10-10 23:25:17 +0300

    Back buttons work in toolbars

--- app/src/main/java/com/hfad/vtb_hack_app/CarSuggestionsActivity.kt
+++ app/src/main/java/com/hfad/vtb_hack_app/CarSuggestionsActivity.kt
@@ -3,9 +3,11 @@ package com.hfad.vtb_hack_app
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_car_suggestions_acivity.*
+import kotlinx.android.synthetic.main.activity_car_to_buy.*
 import kotlinx.android.synthetic.main.item_car_suggestion.*
 
-class CarSuggestionsAcivity : AppCompatActivity() {
+class CarSuggestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_car_suggestions_acivity)
@@ -13,6 +15,15 @@ class CarSuggestionsAcivity : AppCompatActivity() {
         buttonItemCarSuggestionOpen.setOnClickListener(){
             val intent: Intent = Intent(this, CarToBuyActivity::class.java)
             startActivity(intent)
+        }
+
+        setSupportActionBar(carSuggestionsToolbar)
+
+        supportActionBar?.apply {
+            // show back button on toolbar
+            // on back button press, it will navigate to parent activity
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
         }
     }
 }
--- app/src/main/java/com/hfad/vtb_hack_app/CarToBuyActivity.kt
+++ app/src/main/java/com/hfad/vtb_hack_app/CarToBuyActivity.kt
@@ -1,11 +1,22 @@
 package com.hfad.vtb_hack_app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_car_to_buy.*
 
 class CarToBuyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_car_to_buy)
+
+        setSupportActionBar(carToBuyToolbar)
+
+        supportActionBar?.apply {
+            // show back button on toolbar
+            // on back button press, it will navigate to parent activity
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+        }
     }
 }
