23cc69d114ee863eb18c0433c3fc3918850aec31
rikachka <rikachka1@yandex.ru>
2020-10-11 01:28:19 +0300

    Added everything in CreditMoneyCalculationActivity.kt

--- app/src/main/java/com/hfad/vtb_hack_app/CreditMoneyCalculationActivity.kt
+++ app/src/main/java/com/hfad/vtb_hack_app/CreditMoneyCalculationActivity.kt
@@ -2,10 +2,21 @@ package com.hfad.vtb_hack_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_car_to_buy.*
+import kotlinx.android.synthetic.main.activity_credit_money_calculation.*
 
 class CreditMoneyCalculationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_credit_money_calculation)
+
+        setSupportActionBar(creditMoneyToolbar)
+
+        supportActionBar?.apply {
+            // show back button on toolbar
+            // on back button press, it will navigate to parent activity
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+        }
     }
 }
