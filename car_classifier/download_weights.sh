d5b0a03a905c626c9d772e7c3a8e5b9d2422cc70
artli <artliss96@gmail.com>
2020-10-10 22:06:17 +0300

    ml subproblem API

---
+++ car_classifier/download_weights.sh
@@ -0,0 +1,2 @@
+wget https://github.com/spectrico/car-make-model-classifier-yolo3-python/raw/master/model-weights-spectrico-mmr-mobilenet-128x128-344FF72B.pb
+wget https://pjreddie.com/media/files/yolov3.weights -P yolo-coco
