79cf23fd0666ede9fa9739b21279b64e041eaa50
Ivan Pavlov <deltabd33@gmail.com>
2020-10-10 15:53:23 +0300

    Adding photo from gallery

--- app/src/main/java/com/hfad/vtb_hack_app/MainActivity.kt
+++ app/src/main/java/com/hfad/vtb_hack_app/MainActivity.kt
@@ -1,23 +1,19 @@
 package com.hfad.vtb_hack_app
 
-import android.R.attr.bitmap
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.graphics.Bitmap
-import android.os.AsyncTask
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import khttp.post
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.ByteArrayOutputStream
-import java.nio.ByteBuffer
-
-import khttp.post
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import java.io.InputStream
 
 
 //функция отправки запроса
@@ -30,8 +26,10 @@ fun getRecognizedCarTEXT(photo: String) : String {
 }
 
 
+
 class MainActivity : AppCompatActivity() {
     val REQUEST_IMAGE_CAPTURE = 1
+    val REQUEST_GALLERY = 2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -50,6 +48,9 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        buttonFromGallery.setOnClickListener(){
+            dispatchTakePictureFromGalleryIntent()
+        }
 
     }
 
@@ -63,7 +64,7 @@ class MainActivity : AppCompatActivity() {
     //обработка событий меню
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.action_settings->{
+            R.id.action_settings -> {
                 intent = Intent(this, PropertiesActivity::class.java)
                 startActivity(intent)
                 return true
@@ -77,8 +78,61 @@ class MainActivity : AppCompatActivity() {
     //Приняли фотку и обрабатываем ее, переводя в строку и отправляя запрос на сервер
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-            // var recognisedCarData:String=""
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+
+
+
+        if (resultCode == RESULT_OK && data!=null) {
+            when (requestCode){
+
+                REQUEST_GALLERY -> {
+                    val selectedImage: Uri = data?.data!!
+
+                    val imageStream: InputStream? = contentResolver.openInputStream(selectedImage)
+                    val imageBitmap = BitmapFactory.decodeStream(imageStream)
+                    val resized= getResizedBitmap(imageBitmap, 200)//maxSize варьируется
+
+
+
+                    val byteArrayOutputStream = ByteArrayOutputStream()
+                    resized!!.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream)
+                    val byteArray: ByteArray = byteArrayOutputStream.toByteArray()
+                    val photoString = android.util.Base64.encodeToString(
+                        byteArray,
+                        android.util.Base64.DEFAULT
+                    )
+
+                    //ставит фотку во весь экран
+                    imageView.setImageBitmap(resized)
+                    textView.text = photoString
+                    //аналогичный перевод в base64 как при фотографировании выкидывает outOfMemory
+
+                }
+                REQUEST_IMAGE_CAPTURE -> {
+                    val imageBitmap = data?.extras?.get("data") as Bitmap
+
+                    //ставим картиночку в imageView, по сути не нужно
+                    imageView.setImageBitmap(imageBitmap)
+
+                    //перевод фотки в строку
+                    val byteArrayOutputStream = ByteArrayOutputStream()
+                    imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream)
+                    val byteArray: ByteArray = byteArrayOutputStream.toByteArray()
+                    val photoString = android.util.Base64.encodeToString(
+                        byteArray,
+                        android.util.Base64.DEFAULT
+                    )
+                    textView.text = photoString
+                }
+
+
+            }
+
+
+
+
+
+////////////////////////////////////
+            /*
             val imageBitmap = data?.extras?.get("data") as Bitmap
 
             //ставим картиночку в imageView, по сути не нужно
@@ -89,17 +143,32 @@ class MainActivity : AppCompatActivity() {
             imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream)
             val byteArray: ByteArray = byteArrayOutputStream.toByteArray()
             val photoString = android.util.Base64.encodeToString(byteArray ,android.util.Base64.DEFAULT)
+            */
 
-
-
+//////////////////////////
             //неработающая мазафака
-             GlobalScope.launch(Dispatchers.IO) {
-                 var recognisedCarData = getRecognizedCarTEXT(photoString)
-            }
+                // GlobalScope.launch(Dispatchers.IO) {
+                 //var recognisedCarData = getRecognizedCarTEXT(photoString)
+            //}
 
 
-            //textView.text=recognisedCarData
+            //textView.text=photoString
         }
+    }
+
+
+    private fun getResizedBitmap(image: Bitmap, maxSize: Int): Bitmap? {
+        var width = image.width
+        var height = image.height
+        val bitmapRatio = width.toFloat() / height.toFloat()
+        if (bitmapRatio > 1) {
+            width = maxSize
+            height = (width / bitmapRatio).toInt()
+        } else {
+            height = maxSize
+            width = (height * bitmapRatio).toInt()
+        }
+        return Bitmap.createScaledBitmap(image, width, height, true)
     }
 
     //Запускаем камеру и принимаем фотку в MainActivity
@@ -112,9 +181,23 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun dispatchTakePictureFromGalleryIntent() {
+        val takePictureIntent = Intent(Intent.ACTION_PICK)
+        takePictureIntent.type = "image/*"
+        takePictureIntent.action=Intent.ACTION_GET_CONTENT
+        try {
+            startActivityForResult(
+                Intent.createChooser(takePictureIntent, "Pick an image"),
+                REQUEST_GALLERY
+            )
+        } catch (e: ActivityNotFoundException) {
+            // display error state to the user
+        }
+    }
+
 
     //я вообще не помню откуда это взялось
-    private fun takeResponseByBase64(bmpString:String){
+    private fun takeResponseByBase64(bmpString: String){
 
     }
 
