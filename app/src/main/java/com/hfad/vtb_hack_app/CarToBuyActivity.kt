256b8dba08e1f4e56b4a07ba229de356acbad22d
rikachka <rikachka1@yandex.ru>
2020-10-10 23:41:18 +0300

    Added CreditMoneyCalculationActivity

--- app/src/main/java/com/hfad/vtb_hack_app/CarToBuyActivity.kt
+++ app/src/main/java/com/hfad/vtb_hack_app/CarToBuyActivity.kt
@@ -1,14 +1,21 @@
 package com.hfad.vtb_hack_app
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_car_to_buy.*
+import kotlinx.android.synthetic.main.item_car_suggestion.*
 
 class CarToBuyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_car_to_buy)
+
+        buttonCarBuyInCredit.setOnClickListener(){
+            val intent: Intent = Intent(this, CreditMoneyCalculationActivity::class.java)
+            startActivity(intent)
+        }
 
         setSupportActionBar(carToBuyToolbar)
 
---
+++ app/src/main/java/com/hfad/vtb_hack_app/CreditMoneyCalculationActivity.kt
@@ -0,0 +1,11 @@
+package com.hfad.vtb_hack_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreditMoneyCalculationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_credit_money_calculation)
+    }
+}
