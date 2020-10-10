ce2c5b85e44879a5ca70b743a9f3ce784d2b4d7b
rikachka <rikachka1@yandex.ru>
2020-10-10 17:24:23 +0300

    Adding buttons to AboutCarActivity

--- app/src/main/java/com/hfad/vtb_hack_app/AboutCarActivity.kt
+++ app/src/main/java/com/hfad/vtb_hack_app/AboutCarActivity.kt
@@ -1,11 +1,23 @@
 package com.hfad.vtb_hack_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_about_car.*
 
 class AboutCarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_car)
+
+        buttonCarFurther.setOnClickListener(){
+            val intent: Intent = Intent(this, CarNotFoundActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonCarTryAgain.setOnClickListener(){
+            val intent: Intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
